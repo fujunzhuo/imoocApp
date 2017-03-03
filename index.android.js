b7f441344384ce240449dc9351dc2bb5261df914
fujunzhuo <1134476937@qq.com>
2017-03-03 11:07:26 +0800

    'RollerSkatingApp'

--- index.android.js
+++ index.android.js
@@ -10,7 +10,8 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+    ScrollView,
 } from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 /*
@@ -56,52 +57,83 @@ export default class imoocApp extends Component {
     return (
         <TabNavigator
             sceneStyle={[styles.sceneStyle]}
-            tabBarStyle={[styles.tabBarStyle,styles.border]}
-            tabBarShadowStyle={[styles.tabBarShadowStyle,styles.border]}
+            tabBarStyle={[styles.tabBarStyle]}
+            tabBarShadowStyle={[styles.tabBarShadowStyle]}
             hidesTabTouch={false} >
           <TabNavigator.Item
               selected={this.state.selectedTab === 'home'}
               allowFontScaling={false}
-              title="Home"
-              tabStyle={styles.tabStyle}
+              title="首页"
+              tabStyle={[styles.tabStyle]}
               titleStyle={styles.titleStyle}
               selectedTitleStyle={styles.selectedTitleStyle}
               renderBadge={()=>{ return(<View></View>)}}
-              renderIcon={() => <Image source={require('./img/tab/tab_contact_n.png')} />}
-              renderSelectedIcon={() => <Image source={require('./img/tab/tab_contact_p.png')} />}
+              renderIcon={() => <Image source={require('./img/tab/tab_messagecenter_n.png')} style={styles.icon} />}
+              renderSelectedIcon={() => <Image source={require('./img/tab/tab_messagecenter_p.png')} style={styles.icon} />}
               badgeText="1"
               onPress={() => this.setState({ selectedTab: 'home' })}>
-              <Text>homeView</Text>
+              <ScrollView style={{height:1700}}>
+                  <Image source={require('./img/demo/me.png')} />
+                  <Image source={require('./img/demo/newsImg1.png')} />
+                  <Image source={require('./img/demo/newsImg2.png')} />
+                  <Image source={require('./img/demo/slideshow1.jpg')} />
+                  <Image source={require('./img/demo/slideshow2.jpg')} />
+                  <Image source={require('./img/demo/slideshow3.jpg')} />
+                  <Image source={require('./img/demo/slideshow4.jpg')} />
+                  <Image source={require('./img/demo/slideshow5.jpg')} />
+              </ScrollView>
           </TabNavigator.Item>
           <TabNavigator.Item
+              tabStyle={[styles.tabStyle]}
+              titleStyle={styles.titleStyle}
+              selectedTitleStyle={styles.selectedTitleStyle}
               selected={this.state.selectedTab === 'profile'}
-              title="Profile"
-              renderIcon={() => <Image source={require('./img/tab/tab_discovery_n.png')} />}
-              renderSelectedIcon={() => <Image source={require('./img/tab/tab_discovery_p.png')} />}
+              title="推荐"
+              renderIcon={() => <Image source={require('./img/tab/tab_contact_n.png')} style={styles.icon} />}
+              renderSelectedIcon={() => <Image source={require('./img/tab/tab_contact_p.png')} style={styles.icon} />}
               onPress={() => this.setState({ selectedTab: 'profile' })}>
-              <Text>profileView</Text>
+              <View>
+                  <Image source={require('./img/demo/me.png')} />
+                  <Text>profileView</Text>
+              </View>
+
           </TabNavigator.Item>
           <TabNavigator.Item
+              tabStyle={[styles.tabStyle]}
+              titleStyle={styles.titleStyle}
+              selectedTitleStyle={styles.selectedTitleStyle}
               selected={this.state.selectedTab === 'profile1'}
-              title="Profile"
-              renderIcon={() => <Image source={require('./img/tab/tab_messagecenter_n.png')} />}
-              renderSelectedIcon={() => <Image source={require('./img/tab/tab_messagecenter_p.png')} />}
+              title="发现"
+              renderIcon={() => <Image source={require('./img/tab/tab_discovery_n.png')} style={styles.icon} />}
+              renderSelectedIcon={() => <Image source={require('./img/tab/tab_discovery_p.png')} style={styles.icon} />}
               onPress={() => this.setState({ selectedTab: 'profile1' })}>
-              <Text>1231234</Text>
+              <View>
+                  <Image source={require('./img/demo/newsImg1.png')} />
+                  <Text>1231234</Text>
+              </View>
           </TabNavigator.Item>
           <TabNavigator.Item
+              tabStyle={[styles.tabStyle]}
+              titleStyle={styles.titleStyle}
+              selectedTitleStyle={styles.selectedTitleStyle}
               selected={this.state.selectedTab === 'profile2'}
-              title="Profile"
-              renderIcon={() => <Image source={require('./img/tab/tab_myself_n.png')} />}
-              renderSelectedIcon={() => <Image source={require('./img/tab/tab_myself_p.png')} />}
+              title="我的"
+              renderIcon={() => <Image source={require('./img/tab/tab_myself_n.png')} style={styles.icon} />}
+              renderSelectedIcon={() => <Image source={require('./img/tab/tab_myself_p.png')} style={styles.icon} />}
               onPress={() => this.setState({ selectedTab: 'profile2' })}>
-              <Text>sadgasfsad</Text>
+              <View>
+                  <Image source={require('./img/demo/newsImg2.png')} />
+                  <Text>sadgasfsad</Text>
+              </View>
           </TabNavigator.Item>
         </TabNavigator>
     );
   }
 }
-
+var tabIcon = {
+    height:55,
+    widht:55,
+}
 const styles = StyleSheet.create({
     sceneStyle:{
         flex: 1,
@@ -115,13 +147,17 @@ const styles = StyleSheet.create({
         borderStyle:'dashed'
     },
     tabBarStyle:{
-
+        height:tabIcon.height,
+    },
+    icon:{
+        width:30,
+        height:30
     },
     tabBarShadowStyle:{
-
     },
     tabStyle:{
-
+        height:tabIcon.height,
+        width:tabIcon.width,
     },
     titleStyle:{
 
